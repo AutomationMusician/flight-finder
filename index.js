e100528f686dcd9a9584be01eff01632a7a45824
joshuafishman <josh.a.fishman@gmail.com>
2022-09-10 14:09:02 -0400

    Basic website made

--- index.js
+++ index.js
@@ -2,30 +2,38 @@ require('dotenv').config();
 const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
 
 const RAPID_API_KEY = process.env["RAPID_API_KEY"];
 const RAPID_API_HOST = process.env["RAPID_API_HOST"];
 const USE_API = (process.env["USE_API"] == "true");
+const PORT = process.env.PORT || "3000";
 
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': RAPID_API_KEY,
-		'X-RapidAPI-Host': RAPID_API_HOST
-	}
-};
+const app = express();
+app.listen(PORT, () => console.log('listening on port ' + PORT));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded( { extended: true } ));
 
-if (USE_API) {
-    console.log("Using API Mode");
-    // Using this API: https://rapidapi.com/flightlookup/api/timetable-lookup/
-    // for running against the API (will count against the request quota)
-    fetch('https://timetable-lookup.p.rapidapi.com/TimeTable/BOS/LAX/20220906/?7Day=Y', options)
-        .then(response => response.text())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-} else {
-    // Using the test data
-    console.log("Using Test Data");
-    const xml = fs.readFileSync(path.join(__dirname, "result.xml"), 'utf8');
-    console.log(xml);
+async function getData() {
+    if (USE_API) {
+        // Using this API: https://rapidapi.com/flightlookup/api/timetable-lookup/
+        // for running against the API (will count against the request quota)
+
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': RAPID_API_KEY,
+                'X-RapidAPI-Host': RAPID_API_HOST
+            }
+        };
+        const response = await fetch('https://timetable-lookup.p.rapidapi.com/TimeTable/BOS/LAX/20220906/?7Day=Y', options);
+        return await response.text();
+    } else {
+        // Using the test data
+        console.log("Using Test Data");
+        return fs.readFileSync(path.join(__dirname, "result.xml"), 'utf8');
+    }
 }
+
+getData();
