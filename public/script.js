75281496ce8ee41e6598bcc7bb2a151598c324f0
joshuafishman <josh.a.fishman@gmail.com>
2022-09-29 19:57:17 -0400

    Setup result

--- public/script.js
+++ public/script.js
@@ -1,3 +1,7 @@
+function escapeHtml(unsafe) {
+    return unsafe.replaceAll('&', '&amp;').replaceAll('<', '&lt;').replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
+}
+
 function getFlights() {
     const form = document.getElementById("form");
     const formData = new FormData(form);
@@ -7,3 +11,25 @@ function getFlights() {
     const date = formData.get("date");
     fetch(`/api/get-flights/${encodeURIComponent(from)}/${encodeURIComponent(to)}/${encodeURIComponent(date)}/`);
 }
+
+async function addResult(result) {
+    const div = document.createElement("div");
+    div.className = "bubble-container";
+    div.innerHTML =`
+        <div class="bubble">
+            <div class="item">
+                <p>Result</p>
+            </div>
+        </div>
+    `;
+    document.getElementById("response-list").appendChild(div);
+
+
+}
+
+async function getExampleResult() {
+    const resultFetch = await fetch(`/result.json`);
+    return await resultFetch.json();
+}
+
+getExampleResult().then((result) => addResult(result));
